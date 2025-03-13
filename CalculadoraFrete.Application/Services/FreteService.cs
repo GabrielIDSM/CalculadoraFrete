048fe680ae3a02751a627aea16ba485e9b37d170
Gabriel <gabrielinacio@id.uff.br>
2025-03-13 12:06:36 -0300

    Implementação de um Proxy para atender os seguintes objetos:
    - Consumir uma abstração simplificada no front-end ao invés da API da Frenet
    - Adicionar uma segunda camada de validação de dados
    - Esconder o endereço do Client e as credenciais usadas
    
    Posteriormente será possível adicionar recursos de segurança e desempenho

---
+++ CalculadoraFrete.Application/Services/FreteService.cs
@@ -0,0 +1,61 @@
+﻿using CalculadoraFrete.Domain.Entities;
+using CalculadoraFrete.Domain.Interfaces.Integrations;
+using CalculadoraFrete.Domain.Interfaces.Services;
+
+namespace CalculadoraFrete.Application.Services
+{
+    public class FreteService(IFreteIntegrationService freteIntegrationService) : IFreteService
+    {
+        private readonly IFreteIntegrationService _freteIntegrationService = freteIntegrationService;
+
+        public Resposta ObterCotacoes(ParametroEnvio? parametroEnvio)
+        {
+            try
+            {
+                ValidarParametroEnvio(parametroEnvio);
+                List<CotacaoFrete> cotacoes = _freteIntegrationService.ObterCotacoesFrete(parametroEnvio!);
+                return new Resposta()
+                {
+                    Sucesso = true,
+                    ParametroEnvio = parametroEnvio,
+                    Cotacoes = cotacoes
+                };
+            }
+            catch (Exception e)
+            {
+                return new Resposta()
+                {
+                    Sucesso = false,
+                    MensagemErro = e.Message
+                };
+            }
+
+        }
+
+        private void ValidarParametroEnvio(ParametroEnvio? parametroEnvio)
+        {
+            ArgumentNullException.ThrowIfNull(parametroEnvio);
+
+            if (string.IsNullOrWhiteSpace(parametroEnvio.CEPOrigem))
+                throw new Exception("O valor do campo \"CEP de origem\" não pode ser nulo ou vazio");
+
+            if (string.IsNullOrWhiteSpace(parametroEnvio.CEPDestino))
+                throw new Exception("O valor do campo \"CEP de destino\" não pode ser nulo ou vazio");
+
+            if (parametroEnvio.Peso <= 0)
+                throw new Exception("O valor do campo \"Peso do produto\" deve ser maior que 0.0 kg");
+
+            if (parametroEnvio.ValorDeclarado <= 0)
+                throw new Exception("O valor do campo \"Valor declarado\" deve ser maior que R$ 0,00");
+
+            if (parametroEnvio.Largura <= 0)
+                throw new Exception("O valor do campo \"Largura\" deve ser maior que 0 cm");
+
+            if (parametroEnvio.Altura <= 0)
+                throw new Exception("O valor do campo \"Altura\" deve ser maior que 0 cm");
+
+            if (parametroEnvio.Comprimento <= 0)
+                throw new Exception("O valor do campo \"Comprimento\" deve ser maior que 0 cm");
+        }
+    }
+}
---
+++ CalculadoraFrete.Client.Frenet/Services/FrenetIntegrationService.cs
@@ -0,0 +1,75 @@
+﻿using System.Text;
+using CalculadoraFrete.Domain.Entities;
+using CalculadoraFrete.Domain.Interfaces.Integrations;
+using Microsoft.Extensions.Configuration;
+using System.Text.Json;
+
+namespace CalculadoraFrete.Client.Frenet.Services
+{
+    public class FrenetIntegrationService(IConfiguration configuration) : IFreteIntegrationService
+    {
+        private readonly string _shippingUrl = configuration["Frenet:ShippingURL"]!;
+        private readonly string _token = Environment.GetEnvironmentVariable("FRENET_TOKEN")!;
+
+        public List<CotacaoFrete> ObterCotacoesFrete(ParametroEnvio parametroEnvio)
+        {
+            List<CotacaoFrete> cotacoes = [];
+            var body = new
+            {
+                SellerCEP = parametroEnvio.CEPOrigem,
+                RecipientCEP = parametroEnvio.CEPDestino,
+                ShipmentInvoiceValue = parametroEnvio.ValorDeclarado,
+                ShippingItemArray = new List<dynamic>() {
+                    new {
+                        Quantity = 1,
+                        Height = parametroEnvio.Altura,
+                        Length = parametroEnvio.Comprimento,
+                        Weight =  parametroEnvio.Peso,
+                        Width = parametroEnvio.Largura
+                    }
+                },
+                RecipientCountry = "BR"
+            };
+            string bodyAsString = System.Text.Json.JsonSerializer.Serialize(body);
+            StringContent bodyAsStringContent = new(bodyAsString, Encoding.UTF8, "application/json");
+
+            using (HttpClient client = new())
+            {
+                client.DefaultRequestHeaders.Add("accept", "application/json");
+                client.DefaultRequestHeaders.Add("token", _token);
+
+                HttpResponseMessage response = client.PostAsync(_shippingUrl, bodyAsStringContent).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = response.Content.ReadAsStringAsync().Result;
+                    using JsonDocument responseAsJsonDocument = JsonDocument.Parse(responseBody);
+                    JsonElement responseAsJson = responseAsJsonDocument.RootElement;
+                    foreach (JsonElement cotacao in responseAsJson.GetProperty("ShippingSevicesArray").EnumerateArray())
+                    {
+                        bool isErro = cotacao.GetProperty("Error").GetBoolean();
+
+                        if (!isErro)
+                        {
+                            cotacoes.Add(new CotacaoFrete
+                            {
+                                Transportadora = cotacao.GetProperty("Carrier").GetString()!,
+                                CodigoTransportadora = cotacao.GetProperty("CarrierCode").GetString()!,
+                                Servico = cotacao.GetProperty("ServiceDescription").GetString()!,
+                                CodigoServico = cotacao.GetProperty("ServiceCode").GetString()!,
+                                PrazoEntrega = Convert.ToInt32(cotacao.GetProperty("DeliveryTime").GetString()),
+                                Preco = Convert.ToDouble(cotacao.GetProperty("ShippingPrice").GetString())
+                            });
+                        }
+                    }
+                }
+                else
+                {
+                    throw new Exception($"Erro interno");
+                }
+            }
+
+            return cotacoes;
+        }
+    }
+}
