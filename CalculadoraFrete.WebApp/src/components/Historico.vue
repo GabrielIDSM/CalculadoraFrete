c50c74eb72a7e7f23ba63255548dd08971c442bb
Gabriel <gabrielinacio@id.uff.br>
2025-03-16 07:12:06 -0300

    Refatoração parcial dos cards de frete

--- CalculadoraFrete.WebApp/src/components/Historico.vue
+++ CalculadoraFrete.WebApp/src/components/Historico.vue
@@ -1,24 +1,16 @@
 <template>
   <section class="Historico" v-if="freteStore.historico.length">
     <div v-for="(resposta, index) in freteStore.historico" :key="index" class="Frete">
-      <h3>Cotação para envio de {{ resposta.parametroEnvio?.peso }}kg</h3>
-      <p>
-        De: {{ resposta.parametroEnvio?.cepOrigem }} | Para:
-        {{ resposta.parametroEnvio?.cepDestino }}
-      </p>
-
-      <div v-for="cotacao in resposta.cotacoes" :key="cotacao.codigoServico" class="Cotacao">
-        <strong>{{ cotacao.transportadora }} - {{ cotacao.servico }}</strong>
-        <p>
-          Prazo: {{ cotacao.prazoEntrega }} dias | Preço: R$
-          {{ cotacao.preco?.toFixed(2) || '0.00' }}
-        </p>
-      </div>
+      <FreteSucesso v-if="resposta.sucesso && resposta.cotacoes.length" :resposta="resposta" />
+      <FreteErro v-else :resposta="resposta" />
     </div>
   </section>
 </template>
 
 <script setup lang="ts">
+import FreteErro from '@/components/cards/FreteErro.vue'
+import FreteSucesso from '@/components/cards/FreteSucesso.vue'
 import { useFreteStore } from '../stores/useFreteStore'
+
 const freteStore = useFreteStore()
 </script>
---
+++ CalculadoraFrete.WebApp/src/components/cards/FreteErro.vue
@@ -0,0 +1,14 @@
+<template></template>
+
+<script setup lang="ts">
+import type { PropType } from 'vue'
+import type { Resposta } from '@/types/Frete'
+
+const props = defineProps({
+  resposta: {
+    type: Object as PropType<Resposta>,
+    required: true,
+  },
+  index: Number,
+})
+</script>
